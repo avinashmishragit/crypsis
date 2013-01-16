c79bc2c0ca8eb3588bb6beb3aa8859b44f73cf91
inncoentavi <mishra.avinash35@gmail.com>
2013-01-16 16:33:55 +0530

    one to one relationship between table and columns added

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,3 +1,4 @@
 class Column < ActiveRecord::Base
   attr_accessible :data1, :data2, :data3, :data4, :table_id
+  belongs_to :table
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,5 @@
 class Table < ActiveRecord::Base
   attr_accessible :description, :name, :parent_id
   has_many :tables
+  has_one :column
 end
